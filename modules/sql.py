3535c6f3abb9213e8ed4e6c0bdb8a91c6066b29a
Neil Allwood <neilallwood@icloud.com>
2022-11-06 18:54:14 +0000

    Added more to do's to the sql queries file.

--- modules/sql.py
+++ modules/sql.py
@@ -1,7 +1,25 @@
-# --- Insert new article into db:
+# --- Insert a new article into db:
 
 
 # --- Insert new like count for each record:
 
 
 # --- Insert new comment count for each record:
+
+
+# --- Add new user:
+
+
+# --- Change an user password:
+
+
+# --- Disable an user account:
+
+
+# --- Enable an user account:
+
+
+# --- Lock an user account:
+
+
+# --- Unlock an user account:
