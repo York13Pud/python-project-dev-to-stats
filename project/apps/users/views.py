00491aea427731885b5ffff5fe68b6a1aea5db45
York13Pud <neilallwood@icloud.com>
2023-02-01 16:11:46 +0000

    Added logging for the users app

--- project/apps/users/views.py
+++ project/apps/users/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 
+import logging
+
+# --- Settings for the logging configuration:
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'console': {
+            'format': '%(levelname)s:%(asctime)s:%(name)s:%(message)s'
+        },
+        'file': {
+            'format': '%(levelname)s:%(asctime)s:%(name)s:%(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'file',
+            'filename': './apps/users/logs/debug.log'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['file'],
+            'propagate': True,
+        }
+    }
+})
+
+
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 def home_page(request):
     return render(request = request, template_name = "login.html")
