3b63aa3f7aab352e269260d558769340e0fd6423
York13Pud <neilallwood@icloud.com>
2023-02-01 11:44:47 +0000

    Added base templates to list of TEMPLATES

--- project/config/settings.py
+++ project/config/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
