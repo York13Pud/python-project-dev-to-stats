4e4b863dbbdb191dfaed331b3df707813f6d96ea
York13Pud <neilallwood@icloud.com>
2023-02-02 13:43:50 +0000

    Added Profile model to register in admin portal

--- project/apps/users/admin.py
+++ project/apps/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+# --- Register your models here that need to show in the admin portal:
+admin.site.register(Profile)
