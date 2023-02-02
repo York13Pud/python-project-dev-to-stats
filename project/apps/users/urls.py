efce0052cd366f651a6295b646d3159986a25b8d
York13Pud <neilallwood@icloud.com>
2023-02-02 14:58:05 +0000

    Added media path URL

--- project/apps/users/urls.py
+++ project/apps/users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
+
 
 urlpatterns = [
     path(route = "", view = views.home_page, name = "home"),
 ]
+
+
+# --- Add URL path for media files (images etc) from settings:
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
