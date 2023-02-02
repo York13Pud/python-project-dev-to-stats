e282a0ea30e2f0497e8bad0b3da3e86cbad8c79b
York13Pud <neilallwood@icloud.com>
2023-02-02 12:18:58 +0000

    Started creating the Profiles model

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+import uuid
 
 # Create your models here.
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key = True,
+                          default = uuid.uuid4)
+    user = models.OneToOneField(to = User,
+                                on_delete = models.CASCADE,
+                                null = True,
+                                blank = True)
+    
