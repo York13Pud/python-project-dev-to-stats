8aaee3c863138dfcaf0e3558f83d646322f3fca0
York13Pud <neilallwood@icloud.com>
2023-02-03 12:09:59 +0000

    Added test views

--- project/apps/users/views.py
+++ project/apps/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
+from .models import User
 import logging
 
 # --- Settings for the logging configuration:
@@ -40,4 +42,14 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home_page(request):
+    accounts = User.objects.all()
+    #context = {"Users": user}
+    print(accounts[1].__dict__)
+    return render(request = request, template_name = "login.html")
+
+
+def get_users(request):
+    accounts = User.objects.all()
+    #context = {"Users": user}
+    print(accounts.all)
     return render(request = request, template_name = "login.html")
