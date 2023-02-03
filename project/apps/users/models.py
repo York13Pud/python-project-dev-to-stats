85a8ee56e6d60bf73ed9d1d69af6765b96207113
York13Pud <neilallwood@icloud.com>
2023-02-03 12:08:49 +0000

    Changes Users built-in model to add extra fields

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -1,63 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django_cryptography.fields import encrypt
 
+# import uuid
 
-import uuid
+# --- Create the user application models here:
 
-# Create your models here.
-
-class Profile(models.Model):
-    """_summary_
-    This model is used to create a user profile of additional information gathered from dev.to.
-
-    It uses the auth_user table via a one-to-one relationship for the user in the application.
-    """
-    id = models.UUIDField(primary_key = True,
-                          default = uuid.uuid4,
-                          unique = True,
-                          editable = False)
+class User(AbstractUser):
+    """This class will Abstract the built-in django Users model and expand it with the below fields.
+    The login method (username and password) will not change"""
     
-    user = models.OneToOneField(User,
-                                on_delete = models.CASCADE,
-                                null = True,
-                                blank = True)
+    account_id = models.IntegerField(null = True, blank = True)
     
-    account_id = models.IntegerField(null = True,
-                                     blank = True)
+    user_summary = models.CharField(max_length = 2000, null = True, blank = True)
     
-    user_summary = models.CharField(max_length = 2000,
-                                    null = True,
-                                    blank = True)
+    location = models.CharField(max_length = 500, null = True, blank = True)
     
-    location = models.CharField(max_length = 500,
-                                null = True,
-                                blank = True)
+    twitter_username = models.CharField(max_length = 100, null = True, blank = True)
     
-    twitter_username = models.CharField(max_length = 100,
-                                        null = True,
-                                        blank = True)
+    github_username = models.CharField(max_length = 100, null = True, blank = True)
     
-    github_username = models.CharField(max_length = 100,
-                                       null = True,
-                                       blank = True)
+    website_url = models.URLField(max_length = 250, null = True, blank = True)
     
-    website_url = models.URLField(max_length = 250,
-                                  null = True,
-                                  blank = True)
-    
-    profile_image = models.ImageField(null = True, 
-                                      blank = True,
-                                      upload_to = "profiles/",
+    profile_image = models.ImageField(null = True,  blank = True, upload_to = "profiles/",
                                       default = "profiles/default.png")
     
-    joined_on = models.DateTimeField(auto_now_add = False)
+    api_key = encrypt(models.CharField(max_length = 50, null = True, blank = True))
     
-    date_added = models.DateTimeField(auto_now_add = True)
-    
+    joined_on = models.DateTimeField(auto_now_add = False, null = True, blank = True)
+
     def __str__(self):
         """_summary_
-            This returns a string representation of the title in the admin panel for a row, rather than the object description.
+            This returns a string representation of the username in the admin panel for a row, rather than the object description.
         Returns:
-            This returns a string representation of the title in the admin panel for a row, rather than the object description.
+            This returns a string representation of the username in the admin panel for a row, rather than the object description.
         """
-        return str(self.user.username)
+        # --- Username is returned from the Django auth_user table via the one-to-one 
+        # --- relationship from the user field in the Profile model:
+        return str(self.username)
