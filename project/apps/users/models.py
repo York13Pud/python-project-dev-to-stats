52bac8bb694b4e46c743575b9cc93f887c0ab7a3
York13Pud <neilallwood@icloud.com>
2023-02-03 17:42:36 +0000

    Removed some comments that are not needed

--- project/apps/users/models.py
+++ project/apps/users/models.py
@@ -11,22 +11,14 @@ class User(AbstractUser):
     The login method (username and password) will not change"""
     
     account_id = models.IntegerField(null = True, blank = True)
-    
     user_summary = models.CharField(max_length = 2000, null = True, blank = True)
-    
     location = models.CharField(max_length = 500, null = True, blank = True)
-    
     twitter_username = models.CharField(max_length = 100, null = True, blank = True)
-    
     github_username = models.CharField(max_length = 100, null = True, blank = True)
-    
     website_url = models.URLField(max_length = 250, null = True, blank = True)
-    
     profile_image = models.ImageField(null = True,  blank = True, upload_to = "profiles/",
                                       default = "profiles/default.png")
-    
     api_key = encrypt(models.CharField(max_length = 50, null = True, blank = True))
-    
     joined_on = models.DateTimeField(auto_now_add = False, null = True, blank = True)
 
     def __str__(self):
@@ -35,6 +27,5 @@ class User(AbstractUser):
         Returns:
             This returns a string representation of the username in the admin panel for a row, rather than the object description.
         """
-        # --- Username is returned from the Django auth_user table via the one-to-one 
-        # --- relationship from the user field in the Profile model:
+        # --- Username is returned from the users table:
         return str(self.username)
