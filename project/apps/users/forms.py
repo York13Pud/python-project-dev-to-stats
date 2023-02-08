dc45d498bcb2fb86249ed70478deb89b5e7cf038
York13Pud <neilallwood@icloud.com>
2023-02-08 13:43:33 +0000

    Changed CSS class

--- project/apps/users/forms.py
+++ project/apps/users/forms.py
@@ -2,17 +2,29 @@ from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.models import User
 from .models import User
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        # fields = "__all__"
-        exclude = ["groups", "is_superuser", "user_permissions", "last_login", 
-                   "password", "is_staff", "is_active", "last_name",
-                   "date_joined"]
+        
+        fields = ["account_id", 
+                  "username", 
+                  "first_name",
+                  "user_summary",
+                  "location",
+                  "twitter_username", 
+                  "github_username",
+                  "website_url",
+                  "profile_image",
+                  "api_key", 
+                  "joined_on",
+                  "password1"]
+        
         labels = {"first_name": "Dev.To Name"}
+            
         
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         
         for name, field in self.fields.items():
-            field.widget.attrs.update({"class": "input"})
+            field.widget.attrs.update({"class": "form-control"})
