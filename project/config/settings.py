5cbb8c6c12e35b8f5dca7b1e9f56ac5beefbece2
York13Pud <neilallwood@icloud.com>
2023-02-10 14:53:39 +0000

    Added custom MESSAGE_TAGS dict to match BootStrap

--- project/config/settings.py
+++ project/config/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
+
 import environ
 
 
@@ -191,5 +193,15 @@ LOGGING = {
         },
     },
 }
+
+
+# --- Custom alert message tags:
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # %(levelname)s:%(asctime)s:%(name)s:%(message)s
