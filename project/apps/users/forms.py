4dfe4778432134bc0f540504fbf8f593d4efa307
York13Pud <neilallwood@icloud.com>
2023-02-14 19:11:48 +0000

    Added UpdateProfileForm

--- project/apps/users/forms.py
+++ project/apps/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, SetPasswordForm, UserChangeForm
 from .models import User
 
 
@@ -50,3 +50,38 @@ class SetPasswordForm(SetPasswordForm):
     class Meta:
         model = User
         fields = ['new_password1', 'new_password2']
+        
+
+class UpdateProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        
+        fields = ["account_id",  
+                  "first_name",
+                  "user_summary",
+                  "location",
+                  "twitter_username", 
+                  "github_username",
+                  "website_url",
+                  "profile_image"]
+        
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
+            
+        # --- Settings for read only fields:
+        read_only = {"readonly": "readonly",
+                     "class": "form-control-plaintext",
+                     }
+        
+        self.fields["account_id"].widget.attrs = read_only
+        self.fields["first_name"].widget.attrs = read_only
+        self.fields["user_summary"].widget.attrs = read_only
+        self.fields["location"].widget.attrs = read_only
+        self.fields["twitter_username"].widget.attrs = read_only
+        self.fields["github_username"].widget.attrs = read_only
+        self.fields["website_url"].widget.attrs = read_only
+        self.fields["profile_image"].widget.attrs = read_only
+        
