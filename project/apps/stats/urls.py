996fe82fbc5cecbb5105a5b7c3c240f4c9e801d0
York13Pud <neilallwood@icloud.com>
2023-02-16 16:59:51 +0000

    Created URLs for stats app

---
+++ project/apps/stats/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+urlpatterns = [
+    path(route = "", view = views.show_stats, name = "stats"),
+    path(route = "get-published-articles/", view = views.process_published_articles, name = "get_published_articles"),
+]
