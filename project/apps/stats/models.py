fec9264d2ce340216f4ea91e8ed6fdb8fbfd79dc
York13Pud <neilallwood@icloud.com>
2023-02-16 11:12:33 +0000

    Added tags model

--- project/apps/stats/models.py
+++ project/apps/stats/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+# --- Create your models here.
+
+class Tags:
+    """This is the model for the tags that are used in blog articles."""
+    
+    id = models.UUIDField(primary_key = True, 
+                          default = uuid.uuid4, 
+                          unique = True, 
+                          editable = False)
+    name = models.TextField(max_length = 50, 
+                            null = False, 
+                            blank = False)
+    date_added = models.DateTimeField(auto_now_add = True, 
+                                      null = False, 
+                                      blank = False)
+
+    def __str__(self):
+        """_summary_
+            This returns a string representation of the name in the admin panel for a row, rather than the object description.
+        Returns:
+            This returns a string representation of the name in the admin panel for a row, rather than the object description.
+        """
+        # --- Username is returned from the users table:
+        return str(self.name)
