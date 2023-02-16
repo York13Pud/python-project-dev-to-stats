c20e87e748d813171039d00c5e637ef65b498867
York13Pud <neilallwood@icloud.com>
2023-02-16 17:00:13 +0000

    Registered URLs for stats app

--- project/config/urls.py
+++ project/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("stats/", include("apps.stats.urls")),
     path("", include("apps.users.urls"))
 ]
