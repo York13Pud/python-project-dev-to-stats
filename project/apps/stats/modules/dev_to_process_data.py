0c29c97917448f2d9424e95e6773503cad8f153b
York13Pud <neilallwood@icloud.com>
2023-02-18 10:43:55 +0000

    Started script for processing data

---
+++ project/apps/stats/modules/dev_to_process_data.py
@@ -0,0 +1,16 @@
+# --- Import required modules and libraries:
+
+
+# --- Make API call to collect published articles:
+
+
+# --- Check for a HTTP 200 response. If not 200, log and end process:
+
+
+# --- Call function to query articles table to collect all articles:
+
+
+# --- Query article ref ID against each article in API JSON / Pandas return:
+
+
+# --- Query tags table to collect all tag names:
