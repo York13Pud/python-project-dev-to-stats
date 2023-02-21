d00a99f92d0c3e13a042729ead87ae9c447d49e7
York13Pud <neilallwood@icloud.com>
2023-02-21 15:09:27 +0000

    Added add article step to process

--- project/apps/stats/modules/dev_to_process_data.py
+++ project/apps/stats/modules/dev_to_process_data.py
@@ -1,5 +1,5 @@
 # --- Import required modules and libraries:
-from .db_functions import get_all_articles, get_all_tags, check_tag
+from .db_functions import get_all_articles, add_article, get_all_tags, check_tag
 
 def testing_function():
     # --- Make API call to collect published articles:
@@ -10,21 +10,20 @@ def testing_function():
 
     # --- Call function to query articles table to collect all articles:
     all_articles = get_all_articles()
+    
     print(all_articles)
     
     # --- Query article ref ID against each article in API JSON / Pandas return:
 
 
-    # --- call function to query tags table to collect all tag names:
-    all_tags = get_all_tags()
-    print(all_tags)
-
     # --- Call function to check article tags:
-    tags_to_check = [{"name": "Python"},
-                     {"name": "JavaScript"},
-                     {"name": "C#"}]
+    tags_to_check = [{"name": "python"},
+                     {"name": "javascript"},
+                     {"name": "c#"},
+                     {"name": "100daysofcode"},
+                     {"name": "codenewbie"}]
     
-    tag_check = check_tag(tags_to_check = tags_to_check, current_tags = all_tags)
+    tag_check = check_tag(tags_to_check = tags_to_check)
     print(tag_check)
 
     # --- Once tag check is completed, call function to query tags table to collect all tag names
@@ -32,4 +31,6 @@ def testing_function():
     print(all_tags)
     
     # --- and add article to articles table.
+    add_article()
+    
     # --- Then add the tags / article to the article_tags table where needed:
