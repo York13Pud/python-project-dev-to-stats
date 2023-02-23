a78b20af4fed29e9a73113d2847a8b0486258880
York13Pud <neilallwood@icloud.com>
2023-02-23 16:50:44 +0000

    Multiple changes to process_data function

--- project/apps/stats/modules/dev_to_process_data.py
+++ project/apps/stats/modules/dev_to_process_data.py
@@ -3,9 +3,8 @@ from .db_functions import get_all_articles, add_article, get_all_tags, check_tag
 from .dev_to_api_articles import get_published_articles
 from ...users.models import User
 
-import environ
 
-def testing_function():   
+def process_data():   
     
     users = User.objects.values_list("id", "api_key", named=True)
     
@@ -29,37 +28,31 @@ def testing_function():
                 print(f"Response code is {response_code}. It should be 200. Please check your API key is correct.")
             
             else:
-            
                 # --- Call function to query the articles table to collect all known articles:
                 known_articles = get_all_articles()
-                print(known_articles)
                 
-                # --- Query article ref ID against each article in API return:
+                
+                # # --- Query article ref ID against each article in API return: 
                 for published_article in published_articles:
                     if published_article["id"] in known_articles:
                         # # # # Change to log entry
-                        print(f"Article {published_article['id']} is present")
-                        article_id = 
-                        # # --- Proceed to check views, comments and likes count
-                        # add_article_views_count(article_id = str(published_article["id"]), 
-                        #                         article_views = int(published_article["page_views_count"]))
-                        # add_article_likes_count(article_id = str(published_article["id"]), 
-                        #                         article_likes = int(published_article["public_reactions_count"]))
-                        # add_article_comments_count(article_id = str(published_article["id"]), 
-                        #                            article_comments = int(published_article["comments_count"]))
+                        print(f"Article {published_article['id']} is present.")
+                        
+                        # --- Proceed to check views, comments and likes count
+                        add_article_views_count(article_ref_id = int(published_article["id"]), 
+                                                article_views = int(published_article["page_views_count"]))
+                        add_article_likes_count(article_ref_id = int(published_article["id"]), 
+                                                article_likes = int(published_article["public_reactions_count"]))
+                        add_article_comments_count(article_ref_id = int(published_article["id"]), 
+                                                   article_comments = int(published_article["comments_count"]))
                         
                     else:
                         print(f"Article {published_article['id']} is not present")
-                        # --- Get tags from list in article
+                        
+                        # --- Get tags from list in article:
                         tags_to_check = list(published_article['tag_list'])
                         
-                        tag_check = check_tag(tags_to_check = tags_to_check)
-
-                        # # --- Once tag check is completed, call function to query tags table to collect all tag names
-                        # all_tags = get_all_tags()
-                        # print(all_tags)
+                        check_tag(tags_to_check = tags_to_check)
                         
-                        # # --- and add article to articles table.
-                        # add_article()
-                        
-                        # # --- Then add the tags / article to the article_tags table where needed:
+                        # # --- Finally, call the add article to articles table.
+                        add_article(article = published_article)
