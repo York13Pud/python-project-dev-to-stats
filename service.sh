bba217ae6f4e54f398e6444e126165908e1fb647
dev_neil_a <neilallwood@icloud.com>
2023-08-11 18:50:56 +0100

    Updated script

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /opt/python-project-dev-to-stats/venv/bin/activate
-cd /opt/python-project-dev-to-stats/project
-python3 manage.py runserver
+source ~/python-project-dev-to-stats/venv/bin/activate
+cd ~/python-project-dev-to-stats/project
+python3 manage.py runserver 192.168.0.40:8000
