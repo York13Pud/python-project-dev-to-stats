4840378edae28ef13bfd12fc5aa7332a3258ecf3
dev_neil_a <neilallwood@icloud.com>
2023-08-11 18:34:53 +0100

    Added get_followers function

--- project/apps/stats/modules/dev_to_api_articles.py
+++ project/apps/stats/modules/dev_to_api_articles.py
@@ -38,3 +38,41 @@ def get_published_articles(api_key:str, api_endpoint: str):
     response_status_code = response.status_code
 
     return [response_data, response_status_code]
+
+
+def get_followers(api_key:str, api_endpoint: str, page_number: int):
+    """Function:
+
+    Makes an API call to dev.to which pulls the list of published articles for your account.
+    
+    Args:
+        api_key (str): Your unique API key.
+        api_endpoint (str): The URL for the API endpoint.
+
+    Returns:
+        response (Class): Returns the payload of the request in a JSON format.
+    """
+    
+    headers = {"api-key": api_key,
+               "Accept": "application/vnd.forem.api-v1+json"}
+
+    # --- 
+    params = {"per_page": 1000,
+              "page": page_number}
+    
+    # --- Endpoint is: https://dev.to/api/articles/me/published?per_page=1000
+    URL = api_endpoint
+    
+    # --- Execute an API GET request to the API:
+    
+    response = requests.request("GET", url = URL, 
+                            params = params, 
+                            headers = headers)
+
+    response_text = response.text
+    
+    response_data = json.loads(response_text)
+    
+    response_status_code = response.status_code
+
+    return [response_data, response_status_code]
