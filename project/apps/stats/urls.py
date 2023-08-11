4542134f0d35e028071d9efbbad019131f0daa83
dev_neil_a <neilallwood@icloud.com>
2023-08-11 18:32:41 +0100

    Added get-followers URL

--- project/apps/stats/urls.py
+++ project/apps/stats/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path(route = "", view = views.show_stats, name = "stats"),
     path(route = "get-published-articles/", view = views.process_published_articles, name = "get_published_articles"),
+    path(route = "get-followers/", view = views.get_follower_count, name = "get_followers"),
     path(route = "testing/", view = views.testing, name = "testing"),
 ]
